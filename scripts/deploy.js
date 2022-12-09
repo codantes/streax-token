1b4628b801e72ee29156675ad1cdc60c778301d4
unknown <pranavismic@gmail.com>
2022-12-09 19:23:35 +0530

    initial commit

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+const { ethers, network } = require("hardhat");
+
+async function main() {
+  const TokenFactory = await ethers.getContractFactory("StreaXToken");
+  console.log("deploying contract...");
+  const Tokens = await TokenFactory.deploy();
+  console.log(`contract address: ${Tokens.address}`);
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
